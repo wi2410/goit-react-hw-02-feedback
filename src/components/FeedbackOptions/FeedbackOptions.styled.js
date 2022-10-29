f5c2b0ced0a99af3565c6349337ceff02fb645cd
Vadym Vasylenko <101503219+wi2410@users.noreply.github.com>
2022-10-29 20:19:12 +0300

    fix

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled'
 
 export const Button = styled.button`
-    margin:10px;
+    margin: 10px;
     background-color: white;
-    border: 1px solid gray;
-    border-radius: 5px; 
+    border: 1px solid black;
+    border-radius: 4px; 
     cursor: pointer;
-    transition: transform .2s;
     &:hover{
         color: black;
         background-color: grey;
